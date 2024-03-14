a3f7e0b587603d21df3629337b5d6252e8bfff16
CharlesOkechukwu <realcharlieok@gmail.com>
2024-03-14 01:07:41 -0700

    fix nginx limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # increase the amount of traffic Nginx server can handle
 
 exec {'increase limit':
-  command => 'sed -i "s/15/3000" /etc/default/nginx',
+  command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 3000\"/' /etc/default/nginx",
   path    => '/usr/bin:/usr/sbin:/bin'
 }
 
